ece5f18c09efbb3916369d4f35491d9f9557d5b2
dmswetlana <dmswetlana@mail.ru>
2016-01-17 23:25:17 +0300

    first program

---
+++ Page45.cpp
@@ -0,0 +1,28 @@
+// демонстрация возврата функциями значений
+#include<iostream>
+#include<clocale>
+
+using std::cout;
+using std::cin;
+
+int mul(int x, int y);
+
+int main()
+{
+	setlocale(LC_CTYPE, "Russian_Russia.1251");
+
+	int a;
+	a = mul(2, 8);
+	cout<< "значение а=" << a<< "\n";
+
+	a = mul(5, 7);
+	cout << "значение а=" <<a<< "\n";
+
+	system("pause");
+	return 0;
+}
+
+int mul(int x, int y)
+{
+	return x*y;
+}
