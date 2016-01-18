6b26598e1e6e157d966b8b423a8f205aa843114c
dmswetlana <dmswetlana@mail.ru>
2016-01-18 18:17:49 +0300

    Add function cube

--- Page158.cpp
+++ Page158.cpp
@@ -12,10 +12,12 @@ void display2(int num[]);
 void display3(int *num);
 void disp(int num);
 
+void cube(int *n, int num);
+
 int main()
 {
 	setlocale(LC_CTYPE, "Russian_Russia.1251");
-	int t[10], i;
+	int t[10], i, nums[10];
 
 	for (i = 0; i < 10; i++)
 		t[i] = i;
@@ -37,6 +39,18 @@ int main()
 		disp(t[i]);
 	cout << "\n";
 
+	for (i = 0; i < 10; i++)
+		nums[i] = i+1;
+	cout << "Исходное содержимое массива: ";
+	for (i = 0; i < 10; i++)
+		cout << nums[i] << ' ';
+	cout << "\n";
+
+	cube(nums, 10); //вычисляем кубы значений
+	cout << "Изменённое содержимое: ";
+	for (i = 0; i < 10; i++)
+		cout << nums[i] << ' ';
+	cout << "\n";
 
 	system("pause");
 	return 0;
@@ -73,4 +87,14 @@ void display3(int *num)
 void disp(int num)
 {
 		cout << num << " ";
+}
+
+//вычисляем кубы значений
+void cube(int *n, int num)
+{
+	while (num){
+		*n = *n * *n * *n;
+		num--;
+		n++;
+	           }
 }
